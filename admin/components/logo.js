6085a3c4dc89f951bc3dcae07319dced10ab8dc9
Kuma Li <li.jun.kuma@gmail.com>
2020-09-05 18:49:34 +0800

    [admin] delete all contents

--- admin/components/logo.js
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <div className="mb-4 pl-2">
-      <img className="w-4/5" src="/assets/logo.svg" alt="logo" />
-    </div>
-  )
-}
--- admin/lib/api/endpoints.js
+++
@@ -1,5 +0,0 @@
-const subdomain = process.env.NEXT_PUBLIC_ENV;
-const host = `https://${subdomain}.service.tcloudbase.com`;
-export default {
-    login: `${host}/tcb-ext-cms-auth/login`
-}
